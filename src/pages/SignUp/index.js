8ec8d3f3453fc5bcd8f5dee19153c8a8212d87f4
Maycon Francisco <mayconjf@gmail.com>
2019-10-16 15:18:18 -0300

    added SignUp Screen

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,13 +1,61 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
+import PropTypes from 'prop-types';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
-// import { Container } from './styles';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({navigation}) {
   return (
     <Background>
-      <Text>SIGN-IN</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="words"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
