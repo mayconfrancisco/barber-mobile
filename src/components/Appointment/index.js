47a3af9a364fa8c1015f70b4ba1dea2bed1ebfb8
Maycon Francisco <mayconjf@gmail.com>
2019-10-17 14:32:35 -0300

    added Appointment List and Appointment cancellation

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,45 @@
+import React, {useMemo} from 'react';
+import {TouchableOpacity} from 'react-native';
+import {Icon} from 'native-base';
+import {parseISO, formatRelative} from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import {Container, Left, Avatar, Info, Name, Time} from './styles';
+
+export default function Appointment({data, onCancel}) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
+
+  return (
+    <Container past={data.past}>
+      <Left>
+        <Avatar
+          source={{
+            uri: data.provider.avatar
+              ? data.provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${data.provider.name}`,
+          }}
+        />
+
+        <Info>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
+        </Info>
+      </Left>
+
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon
+            type="MaterialIcons"
+            name="event-busy"
+            style={{fontSize: 20, color: '#f64c75'}}
+          />
+        </TouchableOpacity>
+      )}
+    </Container>
+  );
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {Icon} from 'native-base';
 import PropTypes from 'prop-types';
 
+import api from '~/services/api';
+
 import Background from '~/components/Background';
-import {Container} from './styles';
+import Appointment from '~/components/Appointment';
+import {Container, Title, List} from './styles';
 
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('/appointments');
+
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`/appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment,
+      ),
+    );
+  }
+
   return (
     <Background>
       <Container>
-        <Text>Dashboard</Text>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({item}) => (
+            <Appointment data={item} onCancel={() => handleCancel(item.id)} />
+          )}
+        />
       </Container>
     </Background>
   );
