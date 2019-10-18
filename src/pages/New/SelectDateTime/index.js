8031203def93f8aa1aba955f5a6e246b5dbcad5a
Maycon Francisco <mayconjf@gmail.com>
2019-10-18 14:15:24 -0300

    added SelectDateTime

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,20 +1,54 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {TouchableOpacity} from 'react-native';
 import {Icon} from 'native-base';
+import PropTypes from 'prop-types';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
-import {Container} from './styles';
+import {Container, HourList, Hour, Title} from './styles';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({navigation}) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
 
-  console.tron.log('date', date);
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`/providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {provider, time});
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({item}) => (
+            <Hour
+              onPress={() => handleSelectHour(item.value)}
+              enabled={item.available}>
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
@@ -35,3 +69,10 @@ SelectDateTime.navigationOptions = ({navigation}) => ({
     </TouchableOpacity>
   ),
 });
+
+SelectDateTime.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+    getParam: PropTypes.func,
+  }).isRequired,
+};
