99cc436e4583092331363e882de7f9cf8a69582f
Maycon Francisco <mayconjf@gmail.com>
2019-10-18 08:13:08 -0300

    added SelectProvider

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {TouchableOpacity} from 'react-native';
 import {Icon} from 'native-base';
+import PropTypes from 'prop-types';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
+import {Container, ProvidersList, Provider, Avatar, Name} from './styles';
 
-// import { Container } from './styles';
+export default function SelectProvider({navigation}) {
+  const [providers, setProviders] = useState();
 
-export default function SelectProvider() {
-  return <Background />;
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('/providers');
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({item: provider}) => (
+            <Provider
+              onPress={() => navigation.navigate('SelectDateTime', {provider})}>
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatar/50/${provider.name}`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 // headerLeft - function que deve retornar um componente que sera renderizado no lado esquerdo do header do StackNavigator
@@ -26,3 +61,9 @@ SelectProvider.navigationOptions = ({navigation}) => ({
     </TouchableOpacity>
   ),
 });
+
+SelectProvider.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
