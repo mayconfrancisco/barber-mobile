4a213bf7e0493ea65c09262bbefc480f7d68d62e
Maycon Francisco <mayconjf@gmail.com>
2019-10-18 15:01:54 -0300

    added Confirm Appointment

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,73 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useMemo} from 'react';
+import {formatRelative, parseISO} from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+import PropTypes from 'prop-types';
+import {TouchableOpacity} from 'react-native';
+import {Icon} from 'native-base';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-export default function Confirm() {
-  return <View />;
+import Background from '~/components/Background';
+
+import {Container, Avatar, Name, Time, SubmitButton} from './styles';
+
+export default function Confirm({navigation}) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), {locale: pt}),
+    [time],
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}`,
+          }}
+        />
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar Agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.navigationOptions = ({navigation}) => ({
+  title: 'Confirmar agendamento',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}>
+      <Icon
+        type="MaterialIcons"
+        name="chevron-left"
+        style={{fontSize: 20, color: '#fff'}}
+      />
+    </TouchableOpacity>
+  ),
+});
+
+Confirm.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+    getParam: PropTypes.func,
+  }).isRequired,
+};
